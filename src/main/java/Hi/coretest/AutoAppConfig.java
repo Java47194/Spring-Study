7cfd591ea920f0abc009ebb48bb668641b6eadd9
lee <tlkj1@naver.com>
2023-08-04 11:53:08 +0900

    복습17일차

--- src/main/java/Hi/coretest/AutoAppConfig.java
+++ src/main/java/Hi/coretest/AutoAppConfig.java
@@ -19,10 +19,13 @@ public class AutoAppConfig {
     //이름이 같은 수동 빈 과 자동 빈 이 있을경우 수동 빈이 우선권을 갖는다
     //근데 스프링 부트는 같은 빈이 있을경우 실행이 안되게 설정 되어있다. 그럴떈 밑에 설정을 application.properties에 붙이면 된다
     //spring.main.allow-bean-definition-overriding=true
+
+    /*
     @Bean(name = "memoryMemberRepository")
     MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
+    */
 }
 //@ComponentScan
 //classes =  excludeFilters = @Filter(type = FilterType.ANNOTATION , classes = Configuration.class) 붙은 클래스를 뺸다라는 뜻이다.
--- src/main/java/Hi/coretest/member/MemberServiceImpl.java
+++ src/main/java/Hi/coretest/member/MemberServiceImpl.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Component;
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
-    @Autowired // ac.getBean(MemberRepository.class)
+    @Autowired // 생성자 주입(필수값)
+                // 1. 불변 필수 ,
+                // 2.생성자 호출시점에 딱 1번만 호출되는 것이 보장
+                // 3. 생성자가 하나 일경우 @Autowired 생략가능
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
