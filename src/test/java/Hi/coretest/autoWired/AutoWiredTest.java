1585fa94ebcaf09bcfb9b3340956821c069d89b2
lee <tlkj1@naver.com>
2023-08-08 18:42:14 +0900

    복습19일차

--- src/test/java/Hi/coretest/autoWired/AutoWiredTest.java
+++ src/test/java/Hi/coretest/autoWired/AutoWiredTest.java
@@ -18,19 +18,24 @@ public class AutoWiredTest {
 
     static class TestBean{
 
-        @Autowired(required = false)//에러 왜 member가 빈으로 등록 안되있어서 그래서 false로 해야됌
+        @Autowired(required = true)//에러 왜 member가 빈으로 등록 안되있어서 그래서 false로 해야됌
         public void setNoBean1(Member member){
             System.out.println("member1 = " + member);
         }
 
-        @Autowired
-        public void setNoBean2(@Nullable Member member){
-            System.out.println("member2 = " + member); //스프링빈이 없으면 null로 출력
+        @Autowired(required = false)//에러 왜 member가 빈으로 등록 안되있어서 그래서 false로 해야됌
+        public void setNoBean2(Member member){
+            System.out.println("member2 = " + member);
         }
 
         @Autowired
-        public void setNoBean2(Optional<Member> member){
-            System.out.println("member3 = " + member);// 스프링빈이 없으면 Optional.empty로 출력
+        public void setNoBean3(@Nullable Member member){
+            System.out.println("member3 = " + member); //스프링빈이 없으면 null로 출력
+        }
+
+        @Autowired
+        public void setNoBean4(Optional<Member> member){
+            System.out.println("member4 = " + member);// 스프링빈이 없으면 Optional.empty로 출력
         }
 
 
