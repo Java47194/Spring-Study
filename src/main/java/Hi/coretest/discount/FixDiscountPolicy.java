7e205ce971cf357b6cf34a4b212c5d364b42e228
lee <tlkj1@naver.com>
2023-08-12 19:06:26 +0900

    복습23일차

--- src/main/java/Hi/coretest/discount/FixDiscountPolicy.java
+++ src/main/java/Hi/coretest/discount/FixDiscountPolicy.java
@@ -2,7 +2,9 @@ package Hi.coretest.discount;
 
 import Hi.coretest.member.Grade;
 import Hi.coretest.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FixDiscountPolicy implements DiscountPolicy{
     private int discountFixAmount =1000;
     @Override
